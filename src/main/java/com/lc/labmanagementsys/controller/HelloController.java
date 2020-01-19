15384c72f07b7685490375a007290350aacb75aa
suojinxing <2449365663@qq.com>
2020-01-19 09:48:43 +0800

    增加Vue的展示界面。

--- src/main/java/com/lc/labmanagementsys/controller/HelloController.java
+++ src/main/java/com/lc/labmanagementsys/controller/HelloController.java
@@ -1,18 +1,18 @@
 package com.lc.labmanagementsys.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
     @RequestMapping("/hello")
-    public String hello(String name, Model model){
-        model.addAttribute("name",name);
+    public String hello(){
         return "hello";
     }
 
 
-
-//    public String
+    @RequestMapping("/indexUser")
+    public String indexUser(){
+        return "indexUser";
+    }
 }
--- src/main/java/com/lc/labmanagementsys/controller/UserController.java
+++ src/main/java/com/lc/labmanagementsys/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.lc.labmanagementsys.controller;
 
+import com.lc.labmanagementsys.common.JsonResult;
+import com.lc.labmanagementsys.pojo.User;
 import com.lc.labmanagementsys.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -15,5 +21,22 @@ public class UserController {
     public String hello(String name, Model model){
         System.out.println(userService.getRowCount());
         return "hello";
+    }
+
+    @ResponseBody
+    @RequestMapping("/getUsers")
+    public JsonResult getUsers(){
+        JsonResult result = new JsonResult("测试成功");
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("suojinxin8","suojinxing",2));
+        userList.add(new User("suojinxing7","suojinxing",2));
+        userList.add(new User("suojinxingw","suojinxing",2));
+        userList.add(new User("suojinxing6","suojinxing",2));
+        userList.add(new User("suojinxing5","suojinxing",2));
+        userList.add(new User("suojinxing4","suojinxing",2));
+        userList.add(new User("suojinxing3","suojinxing",2));
+        userList.add(new User("suojinxing2","suojinxing",2));
+        result.setData(userList);
+        return result;
     }
 }
